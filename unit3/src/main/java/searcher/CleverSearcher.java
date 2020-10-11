22ca13f2a62e6a6491bcdebed32289712b5bcfd4
tronto88 <U1959998@unimail.hud.ac.uk>
2020-10-11 17:16:52 +0100

    n

--- unit3/src/main/java/searcher/CleverSearcher.java
+++ unit3/src/main/java/searcher/CleverSearcher.java
@@ -30,21 +30,21 @@ public class CleveSearcher extends Searcher {
      *
      * @return kth largest element of array
      */
-    public int findElement() throws IndexingError {
-        int[] array = getArray();
-        int k = getIndex();
-        int[] aux_Array = array[1];
+    public int findElement() throws IndexingError { // starts subroutine to find largest element in an array, if a problem is round it will identify an exception for an error
+        int[] array = getArray(); // uses the getArray function to set an array made up of integers
+        int k = getIndex(); // sets a variable for the largest integer in a set location in an array
+        int[] aux_Array = array[1]; // set an auxiliary array which takes the first indexed integer from array
+        Arrays.sort(array); // sorts the original array variable
 
-        array[1].remove;
-        for (int i =0; i< array.length; i++){
-            array[1] = array[i];
-            if(array[1] < aux_Array[1]){
+        for (int i =0; i< array.length; i++){ // starts a loop, setting the i variable to zero while that variable is less than the array length, adding 1 on each time it loops through
+            array[1] = array[i]; // sets the first element of the original array to match whichever iteration it is through the loop so the second time through the first position will be whatever integer was in the second position of the original array
+            if(array[1] < aux_Array[1]){ // if the current first element of the first element is less than the first element of the auxiliary array
 
-            }else{
-                aux_Array[1] = array[i];
+            }else{ // otherwise
+                aux_Array[1] = array[1]; // replace the first element in the auxiliary array with the first element of the original array
             }
         }
-        System.out.println("the largest number is "+aux_Array[1]);
-        return 0;
+        System.out.println("the largest number is "+aux_Array[1]); // once the variable i has cycled through all of the elements and therefore the array length is less than i, it will show the number remaining in the auxiliary array which is the largest number from the original array
+        return 0; // ends the program
     } // end of clever solution method
 }
